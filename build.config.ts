c4e31080b20aaefc4f0b23f53edb9538887e1c72
Anthony Fu <anthonyfu117@hotmail.com>
2023-07-25 14:58:09 +0200

    feat: minify wasm bundle

--- build.config.ts
+++ build.config.ts
@@ -1,4 +1,6 @@
 import { defineBuildConfig } from 'unbuild'
+import type { Plugin } from 'vite'
+import { transform } from 'esbuild'
 
 export default defineBuildConfig({
   entries: [
@@ -10,5 +12,27 @@ export default defineBuildConfig({
   rollup: {
     emitCJS: false,
     cjsBridge: false,
+  },
+  hooks: {
+    'rollup:options': function (ctx, options) {
+      options.plugins ||= []
+      // @ts-expect-error force
+      options.plugins.push(<Plugin>{
+        name: 'wasm-minify',
+        async renderChunk(code, chunk) {
+          if (chunk.name === 'wasm' && !chunk.fileName.endsWith('.d.ts')) {
+            const result = await transform(code, {
+              minify: true,
+              target: 'es6',
+              minifyIdentifiers: true,
+            })
+            return {
+              code: result.code,
+              map: result.map,
+            }
+          }
+        },
+      })
+    },
   },
 })
